db032c412210ae8a3a3bffd428f95e52f5ad7e8c
arturo-buitrago <arturo.buitrago@gmail.com>
2016-07-14 14:51:44 +0200

    comments again

--- click_files/elements/buffer_flusher.hh
+++ click_files/elements/buffer_flusher.hh
@@ -4,25 +4,35 @@
 #include "elements/standard/simplequeue.hh"
 CLICK_DECLS
 
+/*                  HEADER FILE                 */
+/*This one is the header file for the flusher.  */
+/*It's there pretty much so that click knows what*/
+/*your element does and how to know whether it's*/
+/*being applied correctly                       */
+
 class Buffer_Flusher : public Element { 
 	public:
 		Buffer_Flusher();
 		~Buffer_Flusher();
 		
-
-		//inline Packet* deqyank();
-
-
 		const char *class_name() const	{ return "Buffer_Flusher"; }
+		//Sets the name for the element. Click needs this to find your element.
 		const char *port_count() const	{ return "1/1"; }
+		//Sets the input/output ports.
+		//I thought of having a second output for dropped packages, which is very doable, but decided
+		//that it was ultimately unnecessary.
 		const char *processing() const	{ return "h/lh"; }
+		//This one took a long time to find. Due to the fact that this element works essentially like
+		//a queue, its neither PUSH nor PULL nor AGNOSTIC. This type of processing is undeclared
+		//and I only managed to find it by trawling through the header files of the Queues.
+
+		//Defines the functions that the element has.
 		int configure(Vector<String>&, ErrorHandler*);
 		
 		void push(int, Packet *);
 		Packet* pull(int port);
 	private:
-		//uint32_t maxSize;
-		//SimpleQueue *_q;
+
 };
 
 
